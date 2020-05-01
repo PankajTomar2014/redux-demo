6f60b67b00ccc52f8836b662d3f274c998c29336
Pankajhasmukh2014@gmail.com <pankajhasmukh@2014@gmail.com>
2020-05-01 11:21:36 +0530

    Multiple Reducer combine in one

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 
 const redux = require('redux')              // import redux
 const ReduxStore= redux.createStore         // create  store
-
-const initialState = {                      // State  
+const combineReducer = redux.combineReducers
+const initialStateBooks = {                      // State  
     numberOfBooks:10,
+  
+};
+const initialStatePens = {                   // State     
     numberOfPens:15,   
 };
-
 // const action = {                            // created action
 //     type:'Buy_book',
 //     info:'my first redux programe'
@@ -23,25 +25,33 @@ function buyPen(){                         // call action via buyBook function
         info:'my Second redux programe'
     }
 }
-
 // reducer take two argument such as =-=>>>> reducer(previous_State,action)
-const reducer=( state=initialState,action ) => {
+const bookReducer=( state=initialStateBooks,action ) => {  //first reducer
     switch(action.type){
         case'Buy_book' : return{
             ...state,                                     // clone the state
             numberOfBooks:state.numberOfBooks-1,
-        }
+        }      
+        default :return state;
+    }
+}
+const pensReducer=( state=initialStatePens,action ) => {   //Second reducer
+    switch(action.type){
         case'Buy_Pen' : return{
             ...state,                                     // clone the state
-            numberOfPens:state.numberOfPens-1,
-        }
+            numberOfPens:state.numberOfPens-2,
+        }    
         default :return state;
     }
 }
 // NOW CALL REDUX_STORE BY CALL THE REDUCER
+const reducer= combineReducer({    // combine first and Second reducers in one reducer function
+    Books:bookReducer,          
+    Pen:pensReducer,
+})  
+const store = ReduxStore(reducer)  // pass the main reducer as argument.
 
-const store = ReduxStore(reducer)
-console.log("INITIAL STATE -=>",store.getState());   // allow access the state via getState()
+console.log("INITIAL STATE -=-=>>",store.getState());   // allow access the state via getState()
 const unsubscribe= store.subscribe(()=>{ console.log('UPDATED STATE-=-=>>>',store.getState())  })    // register listner via subscribe(listener)
 store.dispatch(buyBook()); //allow state to be update via dispatch(action)
 store.dispatch(buyBook()); 
